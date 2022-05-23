c967e40d0cc86481e6bdb76b30c532932b758740
Solon Diego Garcia Moreira <sdiegogm@gmail.com>
2022-05-22 22:33:35 -0300

    Update

--- src/app/components/movimentacao-list/movimentacao-list.component.ts
+++ src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -1,16 +1,52 @@
+// movimentacao-list.component.ts
+
 import { Component, OnInit } from '@angular/core';
+import { CorrentistaService } from 'src/app/services/correntista.service';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
 
 @Component({
   selector: 'app-movimentacao-list',
   templateUrl: './movimentacao-list.component.html',
   styleUrls: ['./movimentacao-list.component.css']
 })
+
+
 export class MovimentacaoListComponent implements OnInit {
   movimentacoes:any;
+  correntistas:any;
+  correntista:any;
 
-  constructor() { }
+
+  constructor(private movimentacaoService: MovimentacaoService,
+    private correntistaService: CorrentistaService) { }
+
+    
 
   ngOnInit(): void {
+    this.exibirCorrentistas();
+  }
+  exibirCorrentistas(): void {
+    this.correntistaService.list()
+      .subscribe(
+        data => {
+          this.correntistas = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  listMovimentacoes(): void {
+    this.movimentacaoService.findById(this.correntista.id)
+      .subscribe(
+        data => {
+          this.movimentacoes = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
